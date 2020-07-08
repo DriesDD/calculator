cb68e4d28297076af29caa684ba9dd6f1673a157
DriesDD <driesdedecker@gmail.com>
2020-07-08 12:40:35 +0200

    add other operators and error detection (must-have features complete)

--- calculator.js
+++ calculator.js
@@ -5,7 +5,14 @@ number1 = Number(document.getElementById('number1').value);
 number2 = Number(document.getElementById('number2').value);
 if (document.getElementById('operator').value == '+')
 { result =  number1 + number2 }
-console.log(result);
+else if (document.getElementById('operator').value == '-')
+{ result =  number1 - number2 }
+else if (document.getElementById('operator').value == '*')
+{ result =  number1 * number2 }
+else if (document.getElementById('operator').value == '/')
+{ result =  number1 / number2 };
+if ( (result == 'Infinity') || (result == '-Infinity') || (result ==  'NaN') )
+{ result = 'n/a' };
 };
 
 function show() 
